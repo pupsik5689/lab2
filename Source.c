17e21da1605c1c92716a9ba957cb8d27583e59e6
pupsik5689 <63320400+pupsik5689@users.noreply.github.com>
2021-12-08 10:58:13 +0300

    Add files via upload

--- Source.c
+++ Source.c
@@ -1,78 +1,75 @@
-#include "Header.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-void PrintMas(double* a, int l)
+
+
+int main()
 {
-  int i = 0;
-  for (i = 0; i < l; i++)
-  {
-    printf("%d) %lf\n", i+1, a[i]);
-  }
-}
+  int N, i, f = 0;
+  double l = 0, u = 0, a = 0;
+  FILE* file = 0;
 
-void Puzirec(double* a, int l)
-{
-  int i, j;
-  double tmp = 0;
+  printf("Zhelaete li sami vvseti znachenia?\n1-yes\n0-no\n");
+  scanf_s("%d", &f);
 
-  for (i = 0; i < l; i++)
+  printf("Vvedite kol-vo elementov:\t");
+  scanf_s("%d", &N);
+ /* if (f == 0)
   {
-    for (j = 0; j < l - i - 1; j++)
+    printf("Vvedite minimalnoe znachenie:\t");
+    scanf_s("%lf", &l);
+
+    printf("Vvedite maximalnoe znachenie:\t");
+    scanf_s("%lf", &u);
+
+    if (u <= l)
     {
-      if (a[j] > a[j + 1])
-      {
-        tmp = a[j];
-        a[j] = a[j + 1];
-        a[j + 1] = tmp;
-
-      }
+      printf("!!!ERROR!!! DANNIE NE KORREKTNI\n");
+      return 0;
     }
-  }
-}
+  }*/
 
-void qSort(double* mas, int l)
-{
-  int i = 0;
-  int j = l - 1;
-  double tmp = 0;
-  double x = mas[(int)(l / 2)];
+  
 
-  do
+  
+  if (f == 0)
   {
-    while (mas[i] < x) i++;
-    while (mas[j] > x) j--;
+    printf("Vvedite minimalnoe znachenie:\t");
+    scanf_s("%lf", &l);
 
-    if (i <= j)
+    printf("Vvedite maximalnoe znachenie:\t");
+    scanf_s("%lf", &u);
+
+    if (u <= l)
     {
-      tmp = mas[i];
-      mas[i] = mas[j];
-      mas[j] = tmp;
-      i++;
-      j--;
+      printf("!!!ERROR!!! DANNIE NE KORREKTNI\n");
+      return 0;
     }
 
-  } while (i <= j);
+    fopen_s(&file, "..\\abc.txt", "w");
 
-  if (j > 0)
-    qSort(mas, j + 1);
-
-  if (i < l)
-    qSort(&mas[i], l - i);
-}
-
-void Vstavki(double* mas, int l )
-{
-  int i, j;
-  double tmp = 0.0;
-
-  for (i = 1; i < l; i++)
-  {
-    tmp = mas[i];
-    j = i - 1;
-    while((j >= 0) && (mas[j] > tmp))
+    for (i = 0; i < N; i++)
     {
-      mas[j+1] = mas[j];
-      j--;
+      a = ((double)rand()) / RAND_MAX * (u - l) + l;
+      fprintf(file, "%lf\n", a);
+      
     }
-    mas[j+1] = tmp;
+
+    fclose(file);
   }
+  else
+  {
+    fopen_s(&file, "..\\abc.txt", "w");
+
+    for (i = 0; i < N; i++)
+    {
+      scanf_s("%lf", &a); 
+      
+      fprintf(file, "%lf\n", a);
+    }
+
+    fclose(file);
+
+  }
+  return 0;
 }
